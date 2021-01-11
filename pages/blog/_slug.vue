29e05868330d7f1a87fab515a9e433d24036f427
kanoemon <kanoemon.cho@gmail.com>
2021-01-11 20:02:50 +0900

    Update styles

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,6 +4,12 @@
     <p class="article__meta">{{ formatDate(article.createdAt) }}</p>
     <nuxt-content :document="article" />
 
+    <div class="article__tags">
+      <ul>
+        <li v-for="tag of article.tags" :key="tag">#{{ tag }}</li>
+      </ul>
+    </div>
+
     <prev-next :prev="prev" :next="next" />
   </article>
 </template>
@@ -20,6 +26,7 @@ export default {
   },
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
+    console.log(article);
 
     const [prev, next] = await $content('articles', { deep: true })
       .only(['title', 'slug'])
@@ -36,9 +43,40 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$bottom-space: 60px;
+
 .article {
   line-height: 30px;
+
+  blockquote {
+    position: relative;
+    font-style: italic;
+    background: #efefef;
+    margin: 30px 0;
+    padding: 30px 15px 8px 15px;
+    font-size: 0.9rem;
+  }
+
+  blockquote:before{
+    display: inline-block;
+    position: absolute;
+    top: 5px;
+    left: 3px;
+    content: "“";
+    font-family: sans-serif;
+    color: #cfcfcf;
+    font-size: 70px;
+    line-height: 1;
+}
+
+  .footnotes {
+    font-size: 0.8rem;
+  }  
+
+  .footnotes ol {
+    padding-left: 15px;
+  }  
 
   h2 {
     font-size: 2rem;
@@ -49,7 +87,25 @@ export default {
     margin-bottom: 40px;
     color: #959da5;
     font-weight: 400;
+    font-size: 0.9rem;
+  }
+
+  &__tags {
+    margin-bottom: $bottom-space;
+  }
+
+  &__tags ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+  }
+
+  &__tags li {
+    padding-right: 10px;
   }
 }
 
+.nuxt-content-container {
+  margin-bottom: $bottom-space;
+}
 </style>
